5888462be6f8b9ca39cd2d03a0f1b2a1e70d07c1
root <root@vmi1601511.contaboserver.net>
2024-01-25 15:26:05 -0600

    fix auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -180,10 +180,10 @@ router.post("/login", async (req, res) => {
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    /*if (originalPassword !== req.body.password) {
+    if (originalPassword !== req.body.password) {
       res.status(200).json("Wrong password or username!");
       return;
-    }*/
+    }
 
     const accessToken = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
