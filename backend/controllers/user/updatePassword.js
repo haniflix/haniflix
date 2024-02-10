ad0a31f9a1ad7714ce8cccd23380cb428799f90c
Chibuzo15 <Anyachris85@gmail.com>
2024-02-10 01:32:54 +0100

    User must supply old password in order to change password

--- backend/controllers/user/updatePassword.js
+++ backend/controllers/user/updatePassword.js
@@ -4,6 +4,27 @@ const CryptoJS = require("crypto-js");
 const updatePassword = async (req, res) => {
   if (req.user.id === req.params.id) {
     try {
+      // Retrieve user from the database
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+
+      // Decrypt stored password
+      const decryptedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.SECRET_KEY
+      ).toString(CryptoJS.enc.Utf8);
+
+      // Check if current password matches decrypted password
+      if (decryptedPassword !== req.body.currentPassword) {
+        return res.status(400).json({
+          message: "Current password is incorrect",
+        });
+      }
+
       // Encrypt new password
       const encryptedPassword = CryptoJS.AES.encrypt(
         req.body.newPassword,
@@ -20,12 +41,16 @@ const updatePassword = async (req, res) => {
         },
         { new: true }
       );
-      res.status(200).json("Password updated successfully");
+      res.status(200).json({
+        message: "Password updated successfully",
+      });
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You can update only your account!");
+    res.status(403).json({
+      message: "You can update only your account!",
+    });
   }
 };
 
