0371c687443043ba8735661fa170a33eb34448d6
Topman <okunlolatopman14@gmail.com>
2024-02-24 20:28:23 +0100

    Removed greetings

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,27 +1,22 @@
-import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
 import React, { useState } from "react";
 import NavLogo1 from "../../Assets/Images/Nav-logo.png";
 
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { selectUser, setUser, logout } from "../../store/reducers/auth";
-import { useDispatch } from "react-redux";
+import { logout, selectUser } from "../../store/reducers/auth";
 
 import { useNavigate } from "react-router-dom";
 
-import { useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import { addClassNames } from "../../store/utils/functions";
-import GenresDropdown from "../GenresDropdown";
 
-import styles from "./navbar.module.scss";
 import { useGetUserQuery } from "../../store/rtk-query/usersApi";
+import styles from "./navbar.module.scss";
 
 import {
-  SearchIcon,
-  ProfileIcon,
-  SettingsIcon,
   LogoutIcon,
+  SearchIcon,
+  SettingsIcon
 } from "../../Assets/svgs/tsSvgs";
 
 import ChangeAvatarModal from "../ChangeAvatarModal";
@@ -132,109 +127,12 @@ const Navbar = (props: Props) => {
     // mobile view disabled
     return;
 
-    return (
-      <div
-        id="mobile-header-list"
-        className={addClassNames(showMobileMenu ? "block" : "hidden")}
-      >
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/">
-            <span>Home</span>
-          </Link>
-        </div>
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/series">
-            <span>Series</span>
-          </Link>
-        </div>
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/movies">
-            <span>Movies</span>
-          </Link>
-        </div>
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/new-and-popular">
-            <span>New and Popular</span>
-          </Link>
-        </div>
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/my-list">
-            <span>My List</span>
-          </Link>
-        </div>
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/edit-profile">
-            <span>Edit Profile</span>
-          </Link>
-        </div>
-        <div className="mobile-header-list-item">
-          <Link className="link" to="/settings">
-            <span>Account Settings</span>
-          </Link>
-        </div>
-
-        <div
-          className="mobile-header-list-item"
-          onClick={() => {
-            onLogout();
-          }}
-        >
-          <span>Logout</span>
-        </div>
-      </div>
-    );
   };
 
   const renderMobileMenuToggle = () => {
     // mobile view disabled
     return;
 
-    return (
-      <div className={styles["mobile-header"]}>
-        <div
-          onClick={() => navigate("/")}
-          className="mobile-logo-cont cursor-pointer"
-        >
-          <img src={NavLogo1} alt="" className="mobile-logo" />
-        </div>
-        <div className="flex items-center gap-[5px]">
-          <Link className={styles["link"]} to="/search">
-            <Search className="icon" />
-          </Link>
-          <div className="text-xs">{userData?.username}</div>
-
-          <div>
-            <Notifications className={styles["icon"]} />
-          </div>
-          <Link className="" to="/settings">
-            <div className="bg-gray-300 rounded-[8px] h-[50px] w-[50px]">
-              <img
-                src={
-                  userData?.avatar ? makeImageUrl(userData?.avatar) : NavLogo1
-                }
-                alt=""
-                className="w-full h-full"
-              />
-            </div>
-          </Link>
-          <div
-            className="menu-toggle-container border !p-[8px]"
-            onClick={function () {
-              setShowMobileMenu(!showMobileMenu);
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-              fill="#fff"
-            >
-              <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
-            </svg>
-          </div>
-        </div>
-      </div>
-    );
   };
 
   const renderDesktopMenu = () => {
@@ -323,14 +221,7 @@ const Navbar = (props: Props) => {
               </div>
             </Transition>
           </div>
-          <span
-            className={addClassNames(
-              "ml-[30px] whitespace-nowrap sm:ml-[10px] sm:mr-[10px]",
-              styles["email-text"]
-            )}
-          >
-            Hi, {userData?.username}
-          </span>
+         
 
           <div
             onClick={() => {
