5a60a190184aaf7008a999517db656ae3e05f49d
Joel Onyedika <joelonyedikaepic@gmail.com>
2024-03-08 23:17:00 +0100

    Made changes to the frontend and also made a checkout form for the stripe api

--- frontend/src/components/StripePaymentForm.jsx
+++ frontend/src/components/StripePaymentForm.jsx
@@ -4,34 +4,46 @@ import { CardElement } from "@stripe/react-stripe-js";
 
 function StripePaymentForm({ newUser }) {
   console.log(newUser);
+  
   const [loading, setLoading] = useState(false);
+  const [showCheckoutForm, setShowCheckoutForm] = useState(false);
+  
+
   const stripePromise = loadStripe(import.meta.env.VITE_APP_STRIPE_PUBLIC_KEY);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    
-    const stripe = await stripePromise;
 
-    const { error } = await stripe.redirectToCheckout({
-      lineItems: [{ price: import.meta.env.VITE_APP_STRIPE_SUBSCRIPTION_PRICE_ID, quantity: 1 }],
-      mode: 'subscription',
-      successUrl: 'https://haniflix.com/success',
-      cancelUrl: 'https://haniflix.com/cancel',
-    });
-    
-    if (error) {
-      console.error(error);
-      setLoading(false);
-    }
-  };
+  const handleSubmit = async (event) => {
+  //   fetch("http://localhost:300/api/create-subscription-checkout-session", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     mode: "cors",
+  //     body: JSON.stringify(plan: plan, customerId: userId)
+  //   })
+  //   .then((res) => {
+  //     if(res.ok) return res.json()
+  //     console.log(res)
+  //     return res.json().then((json) => Promise.reject(json))
+  //   })
+  //   .then(({session}) => {
+  //     window.location = session.url
+  //   })
+  //   .catch((e) => {
+  //     console.log(error)
+  //   })
+  // };
+  event.preventDefault()
+  setShowCheckoutForm(true)
 
   return (
     <form onSubmit={handleSubmit}>
       <h4>
         <div style={{ marginBottom: 10, color: '#fff' }}>Card Details:</div>
       </h4>
-      <CardElement options={{ style: { base: { color: '#fff' } } }} />
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
       <button style={{ marginTop: "20px", marginBottom: "20px", color: "#fff" }} type="submit" disabled={loading}>Subscribe</button>
     </form>
   );
